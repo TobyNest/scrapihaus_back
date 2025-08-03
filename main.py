3211214cdbcbece2be38d84e5be911cb6a84b210
Pedro Matumoto <pedromatumoto@gmail.com>
2025-08-03 19:11:07 -0300

    feat: add CORS middleware to FastAPI application

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from beanie import init_beanie
 import motor.motor_asyncio
 from models import Housing  # Importe seu modelo Beanie
@@ -17,6 +18,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # ajuste conforme a porta do seu frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/housings/", response_model=list[Housing])
